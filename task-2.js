be6e24ebf45e854159ef94657fea1db979685451
Muhammad Wasif <wasifmuhammad519@gmail.com>
2024-04-29 18:08:00 +0500

    task2, and 3

---
+++ task-2.js
@@ -0,0 +1,69 @@
+const http = require("http");
+const url = require("url");
+
+const server = http.createServer((req, res) => {
+  const parsedUrl = new URL(req.url, `http://${req.headers.host}`);
+  if (
+    parsedUrl.pathname === "/I/want/title/" &&
+    parsedUrl.searchParams.has("address")
+  ) {
+    const addresses = parsedUrl.searchParams.getAll("address");
+
+    const fetchTitle = (address) => {
+      return new Promise((resolve, reject) => {
+        address = address.replace(/^https?:\/\//, "");
+        http
+          .get(`http://${address}`, (response) => {
+            let body = "";
+
+            response.on("data", (c) => {
+              body += c;
+            });
+
+            response.on("end", () => {
+              const title = body.match(/<title>(.*?)<\/title>/i);
+              resolve({
+                address: address,
+                title: title ? title[1] : "response not getter",
+              });
+            });
+          })
+          .on("error", (e) => {
+            resolve({
+              address: address,
+              title: "NO RESPONSE",
+            });
+          });
+      });
+    };
+
+    Promise.all(addresses.map(fetchTitle))
+      .then((results) => {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.write("<html><head></head><body>");
+        res.write("<h1>Following are the titles of given websites:</h1>");
+        res.write("<ul>");
+        results.forEach((item) => {
+          res.write(`<li>${item.address} - "${item.title}"</li>`);
+        });
+        res.write("</ul></body></html>");
+        res.end();
+      })
+      .catch((error) => {
+        console.error("Error fetching titles:", error);
+        res.writeHead(500);
+        res.write("<h1>Internal Server Error</h1>");
+        res.end();
+      });
+  } else {
+    res.writeHead(404);
+    res.write("<h1>Page not found. Please enter a valid URL.</h1>");
+    res.end();
+  }
+});
+
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
